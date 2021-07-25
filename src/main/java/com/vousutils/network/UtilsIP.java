f680e2274adfd9d699f2e6f58dc7d064dd61b497
Taoning Ge <gtn1024cc@gmail.com>
2021-07-25 12:06:10 +0800

    add UtilsIP. release 0.1.0

--- src/main/java/com/vousutils/network/UtilsIP.java
+++ src/main/java/com/vousutils/network/UtilsIP.java
@@ -1,5 +1,8 @@
 package com.vousutils.network;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
 /**
  * Provide IP convert method.
  */
@@ -18,5 +21,43 @@ public class UtilsIP {
                     + Integer.parseInt(temp[1]) * 256L * 256L + Integer.parseInt(temp[0]) * 256L * 256L * 256L;
         }
         return -1;
+    }
+
+
+    /**
+     * Convert IP String to Hexadecimal
+     * @param ip For example: `192.168.1.1`.
+     * @return Hexadecimal
+     */
+    public static String ipConvertToHex(String ip) {
+//        num ＝ IP_转至10进制 (IP地址)
+//        返回 (进制_十到十六 (num))
+
+        return Long.toHexString(ipConvertToDec(ip));
+    }
+
+    /**
+     * Convert decimal to IP String.
+     * @param dec For example: `3232235777L`.
+     * @return IP String.
+     */
+    public static String decConvertToIP(long dec) {
+        byte[] bytes = ByteBuffer.allocate(Long.BYTES).putLong(dec).array();
+        byte[] bytes1 = Arrays.copyOfRange(bytes, 4, bytes.length);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 3; i++) {
+            sb.append(bytes1[i] & 0xff);
+            sb.append(".");
+        }
+        return sb.substring(0, sb.toString().length() - 1);
+    }
+
+    /**
+     * Convert Hexadecimal to IP String.
+     * @param hex For example: `C0A80101`.
+     * @return IP String.
+     */
+    public static String hexConvertToIP(String hex) {
+        return decConvertToIP(Long.parseLong(hex,16));
     }
 }
--- src/test/java/com/vousutils/network/UtilsIPTest.java
+++ src/test/java/com/vousutils/network/UtilsIPTest.java
@@ -7,14 +7,46 @@ import org.junit.jupiter.api.Test;
 
 public class UtilsIPTest {
     @Test
-    @DisplayName("Test ipConvertToDecimal")
-    void testIpConvertToDecimal() {
-        assertAll("ipConvertToDecimal",
+    @DisplayName("Test ipConvertToDec")
+    void testIpConvertToDec() {
+        assertAll("ipConvertToDec",
                 () -> assertEquals(3232235777L, UtilsIP.ipConvertToDec("192.168.1.1")),
                 () -> assertEquals(16843009L, UtilsIP.ipConvertToDec("1.1.1.1")),
                 () -> assertEquals(167772417L, UtilsIP.ipConvertToDec("10.0.1.1")),
                 () -> assertEquals(4294967295L, UtilsIP.ipConvertToDec("255.255.255.255"))
         );
+    }
 
+    @Test
+    @DisplayName("Test ipConvertToHex")
+    void testIpConvertToHex() {
+        assertAll("ipConvertToHex",
+                () -> assertEquals("C0A80101", UtilsIP.ipConvertToHex("192.168.1.1").toUpperCase()),
+                () -> assertEquals("1010101", UtilsIP.ipConvertToHex("1.1.1.1").toUpperCase()),
+                () -> assertEquals("A000101", UtilsIP.ipConvertToHex("10.0.1.1").toUpperCase()),
+                () -> assertEquals("FFFFFFFF", UtilsIP.ipConvertToHex("255.255.255.255").toUpperCase())
+        );
+    }
+
+    @Test
+    @DisplayName("Test decConvertToIP")
+    void testDecConvertToIP() {
+        assertAll("decConvertToIP",
+                () -> assertEquals("192.168.1.1", UtilsIP.decConvertToIP(3232235777L)),
+                () -> assertEquals("1.1.1.1", UtilsIP.decConvertToIP(16843009L)),
+                () -> assertEquals("10.0.1.1", UtilsIP.decConvertToIP(167772417L)),
+                () -> assertEquals("255.255.255.255", UtilsIP.decConvertToIP(4294967295L))
+        );
+    }
+
+    @Test
+    @DisplayName("Test hexConvertToIP")
+    void testHexConvertToIP() {
+        assertAll("hexConvertToIP",
+                () -> assertEquals("192.168.1.1", UtilsIP.hexConvertToIP("C0A80101")),
+                () -> assertEquals("1.1.1.1", UtilsIP.hexConvertToIP("1010101")),
+                () -> assertEquals("10.0.1.1", UtilsIP.hexConvertToIP("A000101")),
+                () -> assertEquals("255.255.255.255", UtilsIP.hexConvertToIP("FFFFFFFF"))
+        );
     }
 }
